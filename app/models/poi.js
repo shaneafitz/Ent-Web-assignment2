fce1948afb24ce7d0f8fdb05bdf20f6ac84c4e97
Shane Fitzpatrick <fitzpatrick.shane@gmail.com>
2021-05-18 18:54:06 +0100

    Updated assignment 1 to include latitude and longitude

--- app/models/poi.js
+++ app/models/poi.js
@@ -6,6 +6,8 @@ const Schema = Mongoose.Schema;
 const poiSchema = new Schema({
   name: String,
   description: String,
+  latitude: Number,
+  longitude: Number,
   creator: {
     type: Schema.Types.ObjectId,
     ref: "User",
